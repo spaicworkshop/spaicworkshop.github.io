1eb2362aabdfa71bf7c978563d28da2cd647aa40
Alexander Ponticello <alexander.ponticello@cispa.de>
2025-11-13 18:30:22 +0100

    Initial website

---
+++ js/custom/website.js
@@ -0,0 +1,28 @@
+/*jslint browser: true */
+/*global window, $, alert*/
+
+$(document).ready(function () {
+    'use strict';
+    /*
+    $(".smooth-scroll").on('click', 'a', function (e) {
+        // Iterate over all and remove active class
+        $.each($(".nav-item"), function () {
+            $(this).removeClass('active');
+        });
+        // Add active class on clicked <li>
+        $(this).closest(".nav-item").addClass('active');
+        // Below is smoothscrolling
+        var link = $(this).attr('href');
+        // Is there an HTML anchor in the hyperlink?
+        if (link.indexOf("#") >= 0) {
+            e.preventDefault();
+            var hash = link.split('#')[1];
+            $('body,html').animate({
+                scrollTop: $(link).offset().top - 70
+            }, 700);
+            window.location.hash = hash;
+        }
+    });
+    */
+
+});
